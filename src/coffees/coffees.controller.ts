63814693ba4dd32317fe61ce0b46a5c743c6c973
SnowingFox <1503401882@qq.com>
2022-02-10 15:32:59 +0800

    Swagger

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,20 +1,25 @@
-import { Body, Controller, Get, Inject, Param, Patch, Post, Query } from '@nestjs/common'
+import { Body, Controller, Delete, Get, Inject, Param, Patch, Post, Query } from '@nestjs/common'
 import { CoffeesService } from './coffees.service'
 import { CreateCoffeeDto } from './dto/create-coffee.dto'
 import { UpdateCoffeeDto } from './dto/update-coffee.dto'
 import { PaginationQueryDto } from './entities/pagination-query.dto'
 import { Public } from '../common/decorators/public.decorator'
 import { ParseIntPipe } from '../common/pipes/parse-int.pipe'
+import { Protocol } from '../common/decorators/protocol.decorator'
+import { ApiResponse, ApiTags } from '@nestjs/swagger'
 
+@ApiTags('coffees')
 @Controller('coffees')
 export class CoffeesController {
   @Inject()
   private readonly coffeesService: CoffeesService
 
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
   @Public()
   @Get()
-  async findAll(@Query() paginationQuery: PaginationQueryDto) {
-    await new Promise((resolve) => setTimeout(resolve, 5000))
+  async findAll(@Protocol('hi') protocol: string, @Query() paginationQuery: PaginationQueryDto) {
+    // await new Promise((resolve) => setTimeout(resolve, 5000))
+    console.log(protocol)
     return this.coffeesService.findAll(paginationQuery)
   }
 
@@ -31,5 +36,10 @@ export class CoffeesController {
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
     return this.coffeesService.update(id, updateCoffeeDto)
+  }
+
+  @Delete('id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.coffeesService.remove(id)
   }
 }
--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,6 +1,11 @@
-import { Module } from '@nestjs/common'
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common'
 import { APP_GUARD } from '@nestjs/core'
 import { ApiKeyGuard } from './guards/api-key.guard'
+import { LoggingMiddleware } from './middlewares/logging.middleware'
 
 @Module({ providers: [{ provide: APP_GUARD, useClass: ApiKeyGuard }] })
-export class CommonModule {}
+export class CommonModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer.apply(LoggingMiddleware).forRoutes('*')
+  }
+}
